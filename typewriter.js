976f4bf08cf9e5191d13d766e347b1d204c0134a
Charlie C <zbyabqrc@pm.me>
2023-04-24 04:32:01 +0000

    linted

--- typewriter.js
+++ typewriter.js
@@ -1,25 +1,24 @@
-
 // define function to print sentence
-const typewriter = function (sentence, delay) {
+const typewriter = function(sentence, delay) {
   // creatime timer variable to keep track of time
   let timer = delay;
   // loop through each character in sentence
   for (const char of sentence) {
-  // call setTimeout
-  setTimeout(() => {
-    // animated print character to console
-    process.stdout.write(char);
-    // increment timer by x amount each iteration
-  }, timer += 75)
-}
+    // call setTimeout
+    setTimeout(() => {
+      // animated print character to console
+      process.stdout.write(char);
+      // increment timer by x amount each iteration
+    }, timer += 75);
+  }
 
-// call new setTimeout
-setTimeout(() => {
-  // print new line
-  console.log();
-  // timer will have accumulated total delay time at this point 
+  // call new setTimeout
+  setTimeout(() => {
+    // print new line
+    console.log();
+    // timer will have accumulated total delay time at this point
   }, timer);
-}
+};
 
 // call typewriter function with sentence and delay time
-typewriter("hello there from lighthouse labs", 75)
+typewriter("hello there from lighthouse labs", 75);
